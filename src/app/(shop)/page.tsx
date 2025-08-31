f68b2b1371229347b581035edfa91c73573de7ce
JJMoraless <jjmorales.dev@gmail.com>
2025-08-31 17:47:46 -0500

    feat: :sparkles: add init structure of ecommerse

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,15 @@
+import { titleFont } from "@/config/font";
 
-export default function Home() {
+export default function () {
   return (
     <div className="">
       <h1 className="font-bold bg"> hola mundo</h1>
-      <h1>hola mundo 2</h1>
+      <h1 className={`${titleFont.className} font-bold`}>hola mundo 2</h1>
       <button className="btn-primary">aaa</button>
     </div>
   );
 }
+
+
+
+
